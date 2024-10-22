8487955d6190bee58c430418b89fcb350ede00d6
toutestprismemeca <zmenesr@gmail.com>
2024-10-22 16:29:47 +0200

    fix function

--- step1_mainData/url_fix.py
+++ step1_mainData/url_fix.py
@@ -1,6 +1,5 @@
 #traitement URL
 def url_to_clean(df):
-    import numpy as np
     from functions_shared import website_to_clean
     for i,row in df.iterrows():
         if row.loc['url'] is not None:
