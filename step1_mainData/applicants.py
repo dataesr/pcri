213e45c19df40c7e18fc345cb370366cbcf34c0d
toutestprismemeca <zmenesr@gmail.com>
2024-10-25 11:31:43 +0200

    modif proposals

--- step1_mainData/applicants.py
+++ step1_mainData/applicants.py
@@ -2,29 +2,29 @@
 from constant_vars import ZIPNAME, FRAMEWORK
 from config_path import PATH_SOURCE
 
-def role():
-    app1 = app1.loc[app1.project_id.isin(projects.project_id.unique())] 
-    print(f"0 - size app1 sans les exclus: {len(app1)}")
+def role(df):
+
  
     # traitement ROLE
-    if app1['role'].nunique()==5:
-        app1['role'] = app1['role'].str.lower()   
-        app1 = app1.assign(partnerType='applicant')
-        app1.loc[app1.role.isin(['affiliated','associated']),'partnerType'] = app1.role+' partner'
-        app1.loc[app1.role=='host', 'partnerType'] = 'host'        
-        app1.loc[app1['role'] != 'coordinator', 'role'] = 'partner'
+    if df['role'].nunique()==5:
+        df['role'] = df['role'].str.lower()   
+        df = df.assign(partnerType='applicant')
+        df.loc[df.role.isin(['affiliated','associated']),'partnerType'] = df.role+' partner'
+        df.loc[df.role=='host', 'partnerType'] = 'host'        
+        df.loc[df['role'] != 'coordinator', 'role'] = 'partner'
     else:
-        print(f"Attention ! il existe une modalité en plus dans la var ROLE dans les applicants {app1['role'].unique()}")
+        print(f"Attention ! il existe une modalité en plus dans la var ROLE dans les applicants {df['role'].unique()}")
 
+def erc_role():
      #création erc_role
     proj_erc = projects.loc[(projects.stage=='evaluated')&(projects.thema_code=='ERC'), ['project_id', 'destination_code','action_code']]
 #     proj_erc['project_id'] = proj_erc['project_id'].astype('int')
-    app1 = app1.merge(proj_erc, how='left', on='project_id').drop_duplicates()
-    app1 = app1.assign(erc_role='partner')
-    app1.loc[(app1.destination_code=='SyG')&((app1.partnerType=='host')|(app1.role=='coordinator')), 'erc_role'] = 'PI'
-    app1.loc[(app1.destination_code!='SyG')&(app1.role=='coordinator'), 'erc_role'] = 'PI'
-    app1.loc[(app1.destination_code=='ERC-OTHERS'), 'erc_role'] = np.nan
-    app1 = app1.drop(columns=['destination_code', 'action_code']).drop_duplicates()       
+    df = df.merge(proj_erc, how='left', on='project_id').drop_duplicates()
+    df = df.assign(erc_role='partner')
+    df.loc[(df.destination_code=='SyG')&((df.partnerType=='host')|(df.role=='coordinator')), 'erc_role'] = 'PI'
+    df.loc[(df.destination_code!='SyG')&(df.role=='coordinator'), 'erc_role'] = 'PI'
+    df.loc[(df.destination_code=='ERC-OTHERS'), 'erc_role'] = np.nan
+    df = df.drop(columns=['destination_code', 'action_code']).drop_duplicates()       
         
     
     
