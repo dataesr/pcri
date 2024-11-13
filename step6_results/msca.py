b6c5e5d8916733b761653c601397b622c6c28e89
toutestprismemeca <zmenesr@gmail.com>
2024-11-13 09:37:35 +0100

    fix cateory entreprise

--- step6_results/msca.py
+++ step6_results/msca.py
@@ -87,7 +87,7 @@ def msca_entities(me_entities):
             'role', 'participates_as', 'extra_joint_organization', 'is_ejo', 'with_coord',
             'cordis_type_entity_acro', 'cordis_type_entity_code', 'operateur_name',
             'cordis_type_entity_name_en', 'cordis_type_entity_name_fr', 
-            'paysage_category',  'category_woven', 'insee_cat_code', 'insee_cat_name',
+            'paysage_category',  'category_woven', 'entreprise_cat_code', 'entreprise_cat_name',
             'action_code', 'action_name', 'source_id', 'flag_entreprise',
             'entities_name', 'entities_acronym', 'calculated_fund', 'coordination_number', 'number_involved',
             'project_id', 'participation_nuts', 'region_1_name', 'region_2_name', 'regional_unit_name',
