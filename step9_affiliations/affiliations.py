2a8f2ba59159d3bdc7aa3118ee0528e38b439619
zoe <zmenesr@gmail.com>
2025-01-27 16:02:06 +0100

    fix affiliations

--- step9_affiliations/affiliations.py
+++ step9_affiliations/affiliations.py
@@ -25,32 +25,35 @@ def persons_affiliation(perso, entities_all):
         "orcid": row['orcid_id']
         }
 
-        try:
-            if author.get("orcid"):
-            # Get author by Orcid
-                    url = f"https://api.openalex.org/authors/orcid:{author.get('orcid')}?mailto:bso@recherche.gouv.fr"
-                    author_openalex = requests.get(url).json()
-            else:
-                   url = f"https://api.openalex.org/authors?search={author.get('name')}"
-                   if requests.get(url).json().get("results"):
-                          author_openalex = requests.get(url).json().get("results")[0]
-            
-        except requests.exceptions.HTTPError as http_err:
-            print(f"\n{time.strftime("%H:%M:%S")}, {row} -> HTTP error occurred: {http_err}")
-        except requests.exceptions.RequestException as err:
-            print(f"\n{time.strftime("%H:%M:%S")}, {row} -> Error occurred: {err}")                    
-        except Exception as e:
-            print(f"\n{time.strftime("%H:%M:%S")}, {row} -> An unexpected error occurred: {e}")
+    try:
+        if author.get("orcid"):
+        # Get author by Orcid
+            url = f"https://api.openalex.org/authors/orcid:{author.get('orcid')}?mailto=bso@recherche.gouv.fr"
+            author_openalex = requests.get(url).json().get("results")
+            author.update({'display_name':author_openalex.get('display_name'), 'openalex_id':author_openalex.get('id'), 'affiliations':author_openalex.get('affiliations'), 'topics':author_openalex.get('topics'),  'x_concepts':author_openalex.get('x_concepts'), 'ids':author_openalex.get('ids'), 'display_name_alternatives':author_openalex.get('display_name_alternatives')})
+            df=pd.concat([df, pd.json_normalize(author)])
+        else:
+            url = f"https://api.openalex.org/authors?filter=display_name.search:{author.get('name')}"
+            nb_openalex=requests.get(url).json().get("meta").get('count')
+            result={}
+            if nb_openalex>0:
+                for n in range(nb_openalex): 
+                    author_openalex = requests.get(url).json().get("results")[n]
+                    result.update({'display_name':author_openalex.get('display_name'), 'openalex_id':author_openalex.get('id'), 'affiliations':author_openalex.get('affiliations'), 'topics':author_openalex.get('topics'),  'x_concepts':author_openalex.get('x_concepts'), 'ids':author_openalex.get('ids'), 'display_name_alternatives':author_openalex.get('display_name_alternatives')})
+                    author.update(result)
+                    df=pd.concat([df, pd.json_normalize(author)])
+    except requests.exceptions.HTTPError as http_err:
+        print(f"\n{time.strftime("%H:%M:%S")}-> HTTP error occurred: {http_err}")
+    except requests.exceptions.RequestException as err:
+        print(f"\n{time.strftime("%H:%M:%S")}-> Error occurred: {err}")                    
+    except Exception as e:
+        print(f"\n{time.strftime("%H:%M:%S")}-> An unexpected error occurred: {e}")
     
-        if author_openalex:
-            author.update({'affiliations':author_openalex.get('affiliations'), 'topics':author_openalex.get('topics'),  'x_concepts':author_openalex.get('x_concepts'), 'orcid_tmp':author_openalex.get('orcid'), 'display_name_alternatives':author_openalex.get('display_name_alternatives')})
-            result.append(author)
 
     print(time.strftime("%H:%M:%S"))
 
-    r=pd.DataFrame(result)
-    r.loc[r.orcid=='', 'orcid'] = r.orcid_tmp.str.split("/").str[-1]
+    # df.loc[df.orcid=='', 'orcid'] = df.orcid_tmp.str.split("/").str[-1]
     # r2 = r.groupby(['name', 'orcid'])['affiliations'].apply(pd.json_normalize)
-    r2 = r[['name', 'orcid', 'affiliations']]
-    r2.to_pickle(f'{PATH}participants/data_for_matching/persons_author.pkl')
-    return r2
+    # r2 = df[['name', 'orcid', 'affiliations']]
+    df.to_pickle(f'{PATH}participants/data_for_matching/persons_author.pkl')
+    return df
