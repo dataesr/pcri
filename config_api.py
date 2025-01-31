15d99477ed2cf316ec9a52a7e95013e7a9cb53ac
zoe <zmenesr@gmail.com>
2025-01-31 17:02:36 +0100

    change user connect to openalex

--- config_api.py
+++ config_api.py
@@ -5,3 +5,5 @@ sirene_headers = sirene_get_headers(os.environ.get('SIRENE_API_KEY'), os.environ
 scanr_headers = {"Accept":"application/json", 'Authorization': os.environ.get('SCANR_AUTH')}
 paysage_headers = {'Content-Type': 'application/json', 'X-Api-Key': os.environ.get('X-API-KEY')}
 ods_headers = {"Authorization": f"apikey {os.environ.get('ODS_API')}"}
+
+openalex_usermail='zmenesr@gmail.com'
