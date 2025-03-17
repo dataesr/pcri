42d271ffe725a795ab20d04761ec422fbe7af4d8
zoe <zmenesr@gmail.com>
2025-03-17 16:37:39 +0100

    modif func_ent_erc

--- step6_results/msca_erc.py
+++ step6_results/msca_erc.py
@@ -68,17 +68,17 @@ def msca_erc_resume(msca_erc):
     print("### MSCA / ERC evol preparation")
     me_resume = (msca_erc
             .groupby(list(msca_erc.columns.difference(['coordination_number', 'number_involved', 'calculated_fund', 'beneficiary_subv', 'fund_ent_erc'])), dropna=False)
-            .agg({'number_involved':'sum', 'calculated_fund':'sum', 'coordination_number':'sum'})
+            .agg({'number_involved':'sum', 'calculated_fund':'sum', 'coordination_number':'sum','fund_ent_erc':'sum'})
             .reset_index()
-            .rename(columns={'calculated_fund':'funding_part'})
+            .rename(columns={'calculated_fund':'funding_part', 'fund_ent_erc':'funding_entite'})
                 )
 
     # msca_resume.columns
     tot = me_resume.loc[:,~me_resume.columns.str.contains('country|article', regex=True)] 
-    tot = (tot.groupby(list(tot.columns.difference(['coordination_number', 'number_involved', 'funding_part'])), dropna=False)
+    tot = (tot.groupby(list(tot.columns.difference(['coordination_number', 'number_involved', 'funding_part','funding_entite'])), dropna=False)
     #       .groupby(['framework', 'stage', 'call_year', 'project_id', 'role','participates_as', 'thema_code', 'action_code', 'action_name',
     #        'panel_code', 'panel_name', 'destination_code', 'destination_detail_code', 'destination_name_en', 'destination_detail_name_en'], dropna=False)
-        .agg({'funding_part':'sum', 'number_involved':'sum', 'coordination_number':'sum'})
+        .agg({'funding_part':'sum', 'number_involved':'sum', 'coordination_number':'sum', 'funding_entite':'sum'})
         .reset_index()
         .assign(country_code='ALL'))
 
