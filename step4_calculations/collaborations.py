49de46f0f9f36b4cab26349ed0963c04f59d1b78
zoe <zmenesr@gmail.com>
2025-03-17 11:57:47 +0100

    modif country_code and drop columns

--- step4_calculations/collaborations.py
+++ step4_calculations/collaborations.py
@@ -2,12 +2,7 @@ import pandas as pd, numpy as np
 
 def collab_base(tab, stage_value:str):
     print("### COLLAB base")
-    tmp = (tab[(tab['stage']==stage_value)]
-                    .drop(columns={
-        'cordis_type_entity_acro', 'cordis_type_entity_code', 'cordis_type_entity_name_en', 
-        'cordis_type_entity_name_fr', 
-        'cordis_is_sme', 'coordination_number', 'country_code_mapping'})
-                    .rename(columns={'calculated_fund':'fund'}))
+    tmp = tab[(tab['stage']==stage_value)].rename(columns={'calculated_fund':'fund'}).drop(columns='country_code_mapping')
 
     print(f"subv:{'{:,.1f}'.format(tmp['fund'].sum())}")
 
@@ -17,10 +12,7 @@ def collab_base(tab, stage_value:str):
                                 .apply(lambda row:"-".join(row.values.astype(str)), axis=1))
 
     copy = (tab[(tab['stage']==stage_value)]
-                    .drop(columns={
-        'role', 'cordis_is_sme', 'cordis_type_entity_acro',
-        'cordis_type_entity_code', 'cordis_type_entity_name_en',
-        'cordis_type_entity_name_fr',  'coordination_number'})
+                    .drop(columns='role')
                     .add_suffix('_collab')
                     .rename(columns={'project_id_collab':'project_id', 'calculated_fund_collab':'fund_collab'})
                 )
@@ -38,17 +30,25 @@ def collab_cross(i):
                 (i['participates_as']==i['participates_as_collab']))]
                 .groupby(['stage','project_id','country_code', 'participation_nuts','region_1_name', 'extra_joint_organization','country_code_collab',
                         'participation_nuts_collab', 'region_1_name_collab','country_code_mapping_collab', 'participates_as', 'participates_as_collab', 
-                        'extra_joint_organization_collab', 'is_ejo', 'with_coord'], dropna=False)
+                        'extra_joint_organization_collab', 'is_ejo'], dropna=False)
                 .agg({'part_num':'nunique', 'coord_num':'nunique', 'part_num_collab':'nunique', 'fund':'sum', 
                     'fund_collab':'sum'})
                 .reset_index())
 
 def collab(participation, projects, countries):
     print("### COLLABORATIONS")
-    cc = countries.drop(columns=['countryCode', 'country_name_mapping','country_code_mapping']).drop_duplicates()
 
-    collab_eval=collab_base(participation, 'evaluated')
-    collab_signed=collab_base(participation, 'successful')
+    p=(participation
+       .drop(columns=['cordis_type_entity_code', 'cordis_is_sme', 'flag_entreprise', 'erc_role',
+       'cordis_type_entity_name_fr', 'cordis_type_entity_name_en', 'cordis_type_entity_acro', 
+       'coordination_number', 'beneficiary_subv', 'fund_ent_erc', 'with_coord']))
+
+    cc = (countries.drop(columns=['countryCode', 'country_code', 'countryCode_parent'])
+          .drop_duplicates()
+          .rename(columns={'countryCode_iso3':'country_code'}))
+
+    collab_eval=collab_base(p, 'evaluated')
+    collab_signed=collab_base(p, 'successful')
 
     '''COLLAB - general'''
 
